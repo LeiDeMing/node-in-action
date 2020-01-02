32ba210ab32018dda104385d538e9d56312a9fdf
LeiDeMing <136371773@qq.com>
2020-01-02 10:43:09 +0800

    add

---
+++ charpt6/header.js
@@ -0,0 +1,13 @@
+function createHeader(url){
+    const option={
+        url,
+        headers:{
+            'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-charset':'utf-8',
+            'Cache-Control':'max-age=0',
+            'Connection':'keep-alive',
+            'Cookie':'',
+            'Host':''
+        }
+    }
+}
