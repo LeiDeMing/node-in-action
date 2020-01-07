1cb393ea3fd2c4ecf12e83997e663ba14b1f803d
LeiDeMing <136371773@qq.com>
2020-01-07 14:34:20 +0800

    add

---
+++ koa2 and nodejs/charpt5/app.js
@@ -0,0 +1,10 @@
+const Koa = require('koa'),
+    bodyParser = require('koa-bodyparser');
+const app = new Koa(),
+    router = require('./router');
+
+app.use(bodyParser);
+router(app);
+app.listen(3000,()=>{
+    console.log('server is runnig')
+})
---
+++ koa2 and nodejs/charpt5/router.js
@@ -0,0 +1,35 @@
+const router = require('koa-router')();
+module.exports = (app) => {
+    router.get('/', async (ctx, next) => {
+        ctx.response.body = '<h1>index page</h1>';
+    });
+    router.get('/home', async (ctx, next) => {
+        console.log(ctx.request.query);
+        console.log(ctx.request.querystring);
+        ctx.response.body = '<h1>HOME page</h1>';
+    });
+    router.get('/home/:id/:name', async (ctx, next) => {
+        console.log(ctx.params);
+        ctx.response.body = '<h1>HOME page /:id/:name'
+    });
+    router.get('/user', async (ctx, next) => {
+        ctx.response.body = `<form action="/user/login" method="post">
+        <input type="text" name="name" placeholder="请输入用户名 ikcamp"/>
+        <br/>
+        <input type="password" name="password" placeholder="请输入密码"/>
+        <br/>
+        </form>`;
+    });
+    router.post('/user/login', async (ctx, next) => {
+        console.log(ctx.request.body);
+        const { name, password } = ctx.request.body;
+        if (name === 'ikcamp' && password === '123') {
+            ctx.response.body = `Hello ${name}`;
+        }else{
+            ctx.response.body = '账号错误'
+        }
+    });
+    app
+        .use(router.routes())
+        .use(router.allowedMethods())
+}
