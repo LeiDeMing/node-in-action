fee2129c9eebcdc06d1be1af9a84bdc4049674ea
LeiDeMing <136371773@qq.com>
2020-01-08 16:42:40 +0800

    add

--- koa2 and nodejs/charpt8/middleware/index.js
+++ koa2 and nodejs/charpt8/middleware/index.js
@@ -1,5 +1,5 @@
 const miLog = require('./mi-log');
 
 module.exports = (app) => {
-    app.use(miLog)
+    app.use(miLog())
 }
--- koa2 and nodejs/charpt8/middleware/mi-log/logger.js
+++ koa2 and nodejs/charpt8/middleware/mi-log/logger.js
@@ -1,25 +1,31 @@
 const log4js = require('log4js');
+const methods = ['trace','debug','info','warn','error','fatal','mark']
 module.exports = (options) => {
+    const contextLogger = {};
+    log4js.configure({
+        appenders: {
+            cheese: {
+                type: 'file',
+                filename: 'cheese.log'
+            }
+        },
+        categories: {
+            default: {
+                appenders: ['cheese'],
+                level: 'info'
+            }
+        }
+    });
+    const logger = log4js.getLogger('cheese');
     return async (ctx, next) => {
         const startTime = Date.now();
-        log4js.configure({
-            appenders: {
-                cheese: {
-                    type: 'file',
-                    filename: 'cheese.log'
-                }
-            },
-            categories: {
-                default: {
-                    appenders: ['cheese'],
-                    level: 'info'
-                }
-            }
+        methods.forEach((method,i)=>{
+            contextLogger[method] = (message) => logger[method](message)
         });
-        const logger = log4js.getLogger('cheese');
+        ctx.log = contextLogger;
         await next();
         const endTime = Date.now();
         const responseTime = endTime - startTime;
-        logger.info(`响应时间为${responseTime/1000}s`)
+        logger.info(`响应时间为${responseTime/1000}s`);
     }
 }
