98274cf891d05ce6d4797eacae13a167efa541a9
LeiDeMing <136371773@qq.com>
2020-01-08 15:15:07 +0800

    add

--- koa2 and nodejs/charpt7/mocha.spec.js
+++ koa2 and nodejs/charpt7/mocha.spec.js
@@ -8,10 +8,27 @@ const beverage = {
 //         foo.should.be.a('string')
 //     })
 // })
-describe('Array', () => {
-    describe('#indexOf()', () => {
-        it('should return -1 when the value is not present', () => {
-            beverage.tea.indexOf('puer').should.equal(-1);
+// describe('Array', () => {
+//     describe('#indexOf()', () => {
+//         it('should return -1 when the value is not present', () => {
+//             beverage.tea.indexOf('puer').should.equal(-1);
+//         })
+//     })
+// })
+// describe('Asynchronous', () => {
+//     it('done should be executed after 200ms', done => {
+//         const fn = ()=>{
+//             foo.should.be.a('string');
+//             done();
+//         }
+//         setTimeout(fn,200)
+//     })
+// })
+describe('Asynchronous', () => {
+    it('promise', () => {
+        return new Promise(resolve => {
+            foo.should.be.a('string');
+            resolve();
         })
     })
 })
