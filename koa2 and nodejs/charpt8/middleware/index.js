825010ecbc68018c170f3a551445aed6e13d4e2c
LeiDeMing <136371773@qq.com>
2020-01-09 10:18:28 +0800

    add

--- koa2 and nodejs/charpt8/middleware/index.js
+++ koa2 and nodejs/charpt8/middleware/index.js
@@ -3,6 +3,18 @@ const miLog = require('./mi-log');
 const miHttpError = require('./mi-http-error');
 module.exports = (app) => {
     app
-        .use(miHttpError({errorPageFolder:path.resolve(__dirname,'../views/errorPage')}))
+        .use(miHttpError({
+            errorPageFolder: path.resolve(__dirname, '../views/errorPage')
+        }))
         .use(miLog())
+        .on('error', (error, ctx) => {
+            if (ctx && !ctx.headerSent && ctx.status < 500) {
+                ctx.status = 500;
+            }
+            if (ctx && ctx.log && ctx.log.error) {
+                if (!ctx.state.logged) {
+                    ctx.log.error(err.stack);
+                }
+            }
+        })
 }
