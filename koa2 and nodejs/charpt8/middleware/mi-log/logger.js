d3889a98fc5c19a9244a9be6608c0dfa02aaac39
LeiDeMing <136371773@qq.com>
2020-01-09 09:34:09 +0800

    add

--- koa2 and nodejs/charpt8/middleware/mi-log/logger.js
+++ koa2 and nodejs/charpt8/middleware/mi-log/logger.js
@@ -1,31 +1,54 @@
 const log4js = require('log4js');
-const methods = ['trace','debug','info','warn','error','fatal','mark']
+const baseInfo = require('../../config');
+const access = require('./access');
+const methods = ['trace', 'debug', 'info', 'warn', 'error', 'fatal', 'mark']
 module.exports = (options) => {
     const contextLogger = {};
+    const appenders = {};
+    const opts = Object.assign({}, baseInfo, options);
+    const {
+        env,
+        appLogLevel,
+        dir,
+        serverIp,
+        projectName
+    } = opts;
+    const commonInfo = {
+        projectName,
+        serverIp
+    };
+    if (env === 'dev' || evn === 'development') {
+        appenders.out = {
+            type: 'console'
+        }
+    }
+    appenders.cheese = {
+        type: 'dateFile',
+        filename: 'logs/task',
+        pattern: '-yyyy-MM-dd.log',
+        alwaysIncludePattern: true
+    }
     log4js.configure({
-        appenders: {
-            cheese: {
-                type: 'file',
-                filename: 'cheese.log'
-            }
-        },
+        appenders,
         categories: {
             default: {
-                appenders: ['cheese'],
-                level: 'info'
+                appenders: Object.keys(appenders),
+                level: appLogLevel
             }
         }
     });
     const logger = log4js.getLogger('cheese');
     return async (ctx, next) => {
         const startTime = Date.now();
-        methods.forEach((method,i)=>{
-            contextLogger[method] = (message) => logger[method](message)
+        methods.forEach((method, i) => {
+            contextLogger[method] = (message) => logger[method](access(ctx, message, commonInfo))
         });
         ctx.log = contextLogger;
         await next();
         const endTime = Date.now();
         const responseTime = endTime - startTime;
-        logger.info(`响应时间为${responseTime/1000}s`);
+        logger.info(access(ctx, {
+            responseTime: `响应时间为${responseTime/1000}s`
+        }, commonInfo));
     }
 }
